5c7b582c10597d131513c4faed54613e61b38e21
chrisberanek <74572231+chrisberanek@users.noreply.github.com>
2021-03-18 09:52:37 -0500

    Add Ternary to fix page refresh

--- src/Components/School.js
+++ src/Components/School.js
@@ -9,20 +9,25 @@ class SchoolDetail extends Component {
       return school.id == this.props.match.params.id;
     });
 
-    return (
-        <div className='school_container'>
-            <section className='Schools'>
-                <h1>School Details</h1>
-                {schoolDetail.name}<br></br>
-                {schoolDetail.region}<br></br>
-                {schoolDetail.program}<br></br>
-                {schoolDetail.sport}<br></br>
-            </section>
-            <div className='image'>
-                <img src={schoolDetail.img} />
-            </div>
-        </div>
-    );
+      return (
+          <div className='school_container'>
+              {schoolDetail ?
+                  <div>
+                      <section className='Schools'>
+                          <h1>School Details</h1>
+                          {schoolDetail.name}<br></br>
+                          {schoolDetail.region}<br></br>
+                          {schoolDetail.program}<br></br>
+                          {schoolDetail.sport}<br></br>
+                      </section>
+                      <div className='image'>
+                          <img src={schoolDetail.img} />
+                      </div>
+                  </div>
+                  : null
+              }
+          </div>
+      );
  }
 }
 
